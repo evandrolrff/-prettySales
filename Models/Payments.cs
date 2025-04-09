4db13848036a764b9dfda70bad591ddb0817bc98
efilho <efilho@inf.ufsm.br>
2025-04-09 15:43:27 -0300

    added Payments and Sales

---
+++ Models/Payments.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace AtividadeFinal.Models
+{
+    public class Payments
+    {
+        private int id;
+        private User user;
+        private Sales sale;
+        private int amount;
+        private DateTime paymentDate;
+
+        public Payments(int id, User user, Sales sale, int amount, DateTime paymentDate) 
+        {
+            this.id = id;
+            this.user = user;
+            this.sale = sale;
+            this.amount = amount;
+            this.paymentDate = paymentDate;
+        }
+
+        public Payments() { }
+
+        public int Id { get { return id; } set { id = value; } }
+
+        public User User { get { return user; }  set { user = value; } }
+
+        public Sales Sale { get { return sale; } set { sale = value; } }
+
+        public int Amount { get { return amount; } set { amount = value; } }
+
+        public DateTime PaymentDate { get { return paymentDate; } set { paymentDate = value; } }
+
+        private static string ConvertTimeStampToString(DateTime value)
+        {
+            return value.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public Payments FromReader(SQLiteDataReader reader)
+        {
+            Id = reader.GetInt32(reader.GetOrdinal("id"));
+            //User = reader.GetInt32(reader.GetOrdinal("userId"));
+            //Sale = reader.GetInt32(reader.GetOrdinal("saleId")); => PODE SER NULO
+            Amount = reader.GetInt32(reader.GetOrdinal("amount"));
+            PaymentDate = reader.GetDateTime(reader.GetOrdinal("paymentDate"));
+
+            return this;
+        }
+    }
+}
---
+++ Models/Sales.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SQLite;
+using System;
+
+namespace AtividadeFinal.Models
+{
+    public class Sales
+    {
+        private int id;
+        private User user;
+        private Product product;
+        private int quantity;
+        private DateTime saleDate;
+
+        public Sales(int id, User user, Product product, int quantity, DateTime saleDate)
+        {
+            this.id = id;
+            this.user = user;
+            this.product = product;
+            this.quantity = quantity;
+            this.saleDate = saleDate;
+        }
+
+        public Sales() { }
+
+        public int Id { get { return id; } set { id = value; } }
+
+        public User User { get { return user; } set { user = value; } }
+
+        public Product Product { get { return product; } set { product = value; } }
+
+        public int Quantity { get { return quantity; } set { quantity = value; } }
+
+        public DateTime SaleDate { get { return saleDate; } set { saleDate = value; } }
+
+        public Sales FromReader(SQLiteDataReader reader)
+        {
+            Id = reader.GetInt32(reader.GetOrdinal("id"));
+            //User = reader.GetInt32(reader.GetOrdinal("userId"));
+            //Product = reader.GetInt32(reader.GetOrdinal("productId"));
+            Quantity = reader.GetInt32(reader.GetOrdinal("quanity"));
+            SaleDate = reader.GetDateTime(reader.GetOrdinal("saleDate"));
+
+            return this;
+        }
+    }
+}
