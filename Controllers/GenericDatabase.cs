fe3b1fee4a7cb62b96a09ba33cd457401ee005d7
evandrolrff <evandrolrff@gmail.com>
2025-04-10 22:06:57 -0300

    added abstract class for controllers

---
+++ Controllers/GenericDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+namespace AtividadeFinal.Controllers
+{
+    public abstract class GenericDatabase<T> where T : class
+    {
+        protected readonly string connectionString;
+
+        protected GenericDatabase()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["ConnectionBD"].ConnectionString;
+        }
+
+        public abstract List<T> GetAllRegistry();
+
+        public abstract T GetObjectById(int id);
+
+        public abstract bool AddObject(T classT);
+
+        public abstract bool RemoveObject(int id);
+
+        public abstract bool UpdateObject(T classT);
+    }
+}
